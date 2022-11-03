7fa2bf5c7c5ad9f06e08e7175ef3ec8684cea30e
soy9 <67994488+soy9@users.noreply.github.com>
2022-11-03 15:59:46 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,48 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define sz(x) ((int)(x).size())    
-#define f first
-#define s second
+string s;
+char vowel[5] = {'a', 'e', 'i', 'o', 'u'};
 
-typedef unsigned long long ll; 
-const int INF = 987654321;  
-string s; 
-int cnt[30], lcnt, vcnt;
-
-bool isVowel(int idx){
-	return (idx == 0 || idx == 4 || idx == 8 || idx == 14 || idx == 20);
+bool test1() {
+	int vcnt = 0, lcnt = 0;
+	bool flag = 0;
+	for(int i = 0; i < s.size() ; i++){
+		if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+			vcnt++;
+			lcnt = 0;
+			flag = 1;
+		}else{
+			lcnt++;
+			vcnt = 0;
+		}
+		
+		if(i < s.size() - 1 && s[i] == s[i+1]) {
+			if(s[i] != 'e' && s[i] != 'o'){
+				return false;
+			}
+		}
+		
+		if(vcnt == 3 || lcnt == 3){
+			return false;
+		}
+	}
+	if(flag){
+		return true;
+	}else{
+		return false;
+	}
 }
-int main () { 
-	while(true){
-		cin >> s; 
-		if(s == "end"){
-			break;
+int main() {
+	cin >> s;
+	while(s != "end"){
+		if(test1()) {
+			cout << "<" << s << "> is acceptable.\n";
+		} else {
+			cout << "<" << s << "> is not acceptable.\n";
 		}
-		memset(cnt, 0, sizeof(cnt));
-		lcnt = vcnt = 0;
-		bool flag = 0;
-		int prev = -1;
-		for(int i = 0; i < s.size(); i++){
-			int idx = s[i] - 'a'; 
-			cnt[idx]++; 
-			if(isVowel(idx)){
-				lcnt++, vcnt = 0;
-			}
-			else {
-				vcnt++, lcnt = 0;
-			}
-			if(vcnt == 3 || lcnt == 3) {
-				flag = 1; 
-			}
-			if(i >= 1 && (s[i - 1] == s[i]) && (idx != 4 && idx != 14)){
-				flag  = 1;
-			} 
-		}
-		if(!cnt[0] && !cnt[4] && !cnt[8] && !cnt[14] && !cnt[20]){
-			flag = 1; 
-		}
-		if(flag) cout << "<" << s << ">" << " is not acceptable.\n";
-		else cout << "<" << s << ">" << " is acceptable.\n";
-	} 
+		s.clear();
+		cin >> s;
+	}
 	return 0;
 }
