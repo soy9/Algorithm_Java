6e6f39b7949416ac886051d1c2f8a7688bc54279
soy9 <67994488+soy9@users.noreply.github.com>
2023-02-02 01:39:33 +0900

    [Bronze II] Title: 블랙잭, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,22 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n, m, max = 0;
-		int[] arr = null;
-		int tmp;
-		n = sc.nextInt();
-		m = sc.nextInt();
-		arr = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int tmp, max = 0;
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+
+		st = new StringTokenizer(br.readLine(), " ");
 
 		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
 		for (int i = 0; i < n - 2; i++) {
 			for (int j = i + 1; j < n - 1; j++) {
+				if (arr[i] + arr[j] >= m) {
+					continue;
+				}
 				for (int k = j + 1; k < n; k++) {
 					tmp = arr[i] + arr[j] + arr[k];
 					if (tmp <= m && tmp > max) {
