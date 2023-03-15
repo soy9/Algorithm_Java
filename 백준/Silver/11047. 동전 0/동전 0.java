af0c5b18b5d351f74d6346e2bde653ad820863f3
soy9 <67994488+soy9@users.noreply.github.com>
2023-03-16 00:02:41 +0900

    [Silver IV] Title: 동전 0, Time: 80 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,32 +1,29 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		int K = in.nextInt();
-		
-		int[] coin = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			coin[i] = in.nextInt();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int[] money = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			money[i] = Integer.parseInt(br.readLine());
 		}
-		
-		int count = 0;
- 
-		for(int i = N - 1; i >= 0; i--) {
- 
-			// 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
-			if(coin[i] <= K) {
-				// 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
-				count += (K / coin[i]);
-				K = K % coin[i];
+		Arrays.sort(money);
+
+		int cnt = 0;
+		for (int i = N - 1; i >= 0; i--) {
+			if (K >= money[i]) {
+				cnt += K / money[i];
+				K %= money[i];
 			}
 		}
-		System.out.println(count);
+
+		System.out.println(cnt);
 	}
- 
 }
