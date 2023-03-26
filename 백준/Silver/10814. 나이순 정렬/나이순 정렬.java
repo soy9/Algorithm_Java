fe04d58cd9c6b60625cad31a02e21ec2356a796b
soy9 <67994488+soy9@users.noreply.github.com>
2023-03-27 00:02:17 +0900

    [Silver V] Title: 나이순 정렬, Time: 628 ms, Memory: 62188 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,36 +1,49 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Comparator;
- 
-public class Main {
-	public static void main(String[] args) {		
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		String[][] arr = new String[N][2];
-		
- 
-		for(int i = 0; i < N; i++) {
-			arr[i][0] = in.next();	// 나이
-			arr[i][1] = in.next();	// 이름
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	static class Person implements Comparable<Person> {
+		int age;
+		int n;
+		String name;
+
+		public Person(int age, int n, String name) {
+			super();
+			this.age = age;
+			this.n = n;
+			this.name = name;
 		}
- 
-		
-		Arrays.sort(arr, new Comparator<String[]>() {
-			// 나이순으로 정렬
-			@Override
-			public int compare(String[] s1, String[] s2) {
-				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+
+		@Override
+		public int compareTo(Person o) {
+			if (this.age == o.age) {
+				return Integer.compare(this.n, o.n);
 			}
-			
-		});
-		
-		for(int i = 0; i < N; i++) {
-			System.out.println(arr[i][0] + " " + arr[i][1]);
+			return Integer.compare(this.age, o.age);
 		}
-        
-		
+
 	}
- 
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		List<Person> people = new LinkedList<Person>();
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int age = Integer.parseInt(st.nextToken());
+			String name = st.nextToken();
+			people.add(new Person(age, i, name));
+		}
+		
+		Collections.sort(people);
+		
+		for(Person p : people) {
+			sb.append(p.age + " " + p.name + "\n");
+		}
+		System.out.println(sb);
+	}
 }
