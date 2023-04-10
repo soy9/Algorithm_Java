db1c2de7d50acfe1fe65c71294426c7b509cf896
soy9 <67994488+soy9@users.noreply.github.com>
2023-04-10 23:28:36 +0900

    [Bronze V] Title: 구구단, Time: 76 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= 9; i++) {
+			sb.append(N + " * " + i + " = " + (N * i) + "\n");
+		}
+
+		System.out.println(sb);
+	}
+}
