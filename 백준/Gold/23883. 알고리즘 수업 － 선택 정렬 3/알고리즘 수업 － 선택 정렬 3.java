314114317449aac85f6d9ec5696ec7850259641b
soy9 <67994488+soy9@users.noreply.github.com>
2024-06-24 16:17:45 +0900

    [Gold IV] Title: 알고리즘 수업 - 선택 정렬 3, Time: 1604 ms, Memory: 150972 KB -BaekjoonHub

---
+++ 백준/Gold/23883. 알고리즘 수업 － 선택 정렬 3/알고리즘 수업 － 선택 정렬 3.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        TreeMap<Integer, Integer> idxMap = new TreeMap<>(Collections.reverseOrder());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            int val = Integer.parseInt(st.nextToken());
+            arr[i] = val;
+            idxMap.put(val, i);
+        }
+
+        // selection sort
+        int times = 0;
+        int i = N;
+        for(Integer key : idxMap.keySet()) {
+            
+            if(arr[i] == key) {
+                i--;
+                continue;
+            }
+
+            int idx = idxMap.get(key);
+            int temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+
+            idxMap.replace(key, i);
+            idxMap.replace(arr[idx], idx);
+
+            if(++times == K) {
+                System.out.println(arr[idx] + " " + arr[i]);
+                break;
+            }
+            i--;
+
+        }
+
+        if(times < K) {
+            System.out.println("-1");
+        }
+
+    }
+}
