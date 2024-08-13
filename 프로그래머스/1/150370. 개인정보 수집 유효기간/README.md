f5969c62dcaeee629898a55bca99fb22bd944e91
soy9 <67994488+soy9@users.noreply.github.com>
2024-08-13 14:48:43 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.28 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
 
 ### 성능 요약
 
-메모리: 70.6 MB, 시간: 1.87 ms
+메모리: 78.8 MB, 시간: 4.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 13일 14:38:38
+2024년 08월 13일 14:48:42
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = {};
-        List<Integer> list = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
         int DATE_LEN = 10;
    
         int todayDate = Integer.parseInt(today.replace(".", ""));
@@ -35,7 +35,6 @@ class Solution {
                     endMonth = 12;
                     addYear --;
                 }
-            
                 endYear += addYear;
             }
             
@@ -43,18 +42,18 @@ class Solution {
             int endDate = (endYear*10000) + (endMonth*100) + Integer.parseInt(days[2]);
             
             if(endDate <= todayDate) {
-                list.add(idx);
+                answer.add(idx);
             }
             idx++;
         }
         
-        // 오름차순 정렬
-        Collections.sort(list);
-        answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+//         // 오름차순 정렬
+//         Collections.sort(list);
+//         answer = new int[list.size()];
+//         for(int i = 0; i < list.size(); i++) {
+//             answer[i] = list.get(i);
+//         }
         
-        return answer;
+        return answer.stream().mapToInt(integer -> integer).toArray();
     }
 }
