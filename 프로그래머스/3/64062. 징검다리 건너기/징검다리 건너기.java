281324731a0dff392c184caac9f049cfe581e624
김소이 <67994488+soy9@users.noreply.github.com>
2025-05-27 11:22:08 +0900

    [level 3] Title: 징검다리 건너기, Time: 58.28 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    
+    boolean canJump(int mid, int[] stones, int k) {
+        int cnt = 0;
+        
+        for(int stone : stones) {
+            if(stone - mid < 0) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            
+            if(cnt == k) {
+                return false;
+            }
+        }
+        
+        return true;        
+    }
+        
+    public int solution(int[] stones, int k) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        int mid = 0;
+        
+        for(int stone : stones) {
+            min = Math.min(min, stone);
+            max = Math.max(max, stone);
+        }
+        
+        
+        while (min < max) {
+            mid = (min + max + 1) / 2;
+                        
+            if(canJump(mid, stones, k)) {
+                min = mid;            
+            } else {
+                max = mid - 1;
+            }
+        }
+        
+        return max;
+    }
+}
