f98d91495b46f9ec3403443dd50797c0bcfb0025
김소이 <67994488+soy9@users.noreply.github.com>
2025-06-18 23:06:43 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		int N = Integer.parseInt(br.readLine());
+        
+        long[] dp = new long[N + 1];
+        dp[1] = 1;
+        
+        for(int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+		// output
+		System.out.println(dp[N]);
+
+	}
+}
